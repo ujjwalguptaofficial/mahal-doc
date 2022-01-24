2e1f228e48ea30b56a4883f493530f9064b1ae1f
Ujjwal Gupta <ujjwalkumargupta4u@gmail.com>
2022-01-24 19:34:30 +0530

    create basic folder

--- content/docs/index.js
+++ content/docs/index.js
@@ -4,56 +4,63 @@ module.exports = [
     url: 'get-started',
   },
   {
-    text: 'Component',
-    url: 'component',
-  },
-  {
-    text: 'State',
-    url: 'state',
-  },
-  {
-    text: 'Component Lifecycle',
-    url: 'component-lifecycle',
-  },
-  {
-    text: 'Reactive',
-    url: 'reactive',
-  },
-  {
-    text: 'Computed',
-    url: 'computed',
-  },
-  {
-    text: 'Prop',
-    url: 'prop',
-  },
-  {
-    text: 'Event',
-    url: 'event',
-  },
-  {
-    text: 'Model',
-    url: 'model',
-  },
-  {
-    text: 'Conditional',
-    url: 'conditional',
-  },
-  {
-    text: 'List',
-    url: 'list',
-  },
-  {
-    text: 'Conditional class',
-    url: 'conditional-class',
-  },
-  {
-    text: 'Directive',
-    url: 'directive',
-  },
-  {
-    text: 'Formatter',
-    url: 'formatter',
+    text: 'Basic',
+    url: 'basic',
+    expand: true,
+    children: [
+      {
+        text: 'Component',
+        url: 'component',
+      },
+      {
+        text: 'State',
+        url: 'state',
+      },
+      {
+        text: 'Component Lifecycle',
+        url: 'component-lifecycle',
+      },
+      {
+        text: 'Reactive',
+        url: 'reactive',
+      },
+      {
+        text: 'Computed',
+        url: 'computed',
+      },
+      {
+        text: 'Prop',
+        url: 'prop',
+      },
+      {
+        text: 'Event',
+        url: 'event',
+      },
+      {
+        text: 'Model',
+        url: 'model',
+      },
+      {
+        text: 'Conditional',
+        url: 'conditional',
+      },
+      {
+        text: 'List',
+        url: 'list',
+      },
+      {
+        text: 'Conditional class',
+        url: 'conditional-class',
+      },
+      {
+        text: 'Directive',
+        url: 'directive',
+      },
+      {
+        text: 'Formatter',
+        url: 'formatter',
+      }
+    ]
   },
   {
     text: 'Advanced',
