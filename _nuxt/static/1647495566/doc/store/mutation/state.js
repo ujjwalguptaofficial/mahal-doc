369310f0b2d29474341f807f97af2d8b23f4bed3
ujjwalguptaofficial <ujjwalkumargupta4u@gmail.com>
2022-03-18 12:26:51 +0000

    Deploying to gh-pages from @ ujjwalguptaofficial/mahal-doc@175c78b93b0035bd28109b42f88fcc7d420b6f8b 🚀

--- _nuxt/static/1647495566/doc/store/mutation/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmahal-doc\u002F_nuxt\u002Fstatic\u002F1647495566",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{_app:{basePath:"\u002Fmahal-doc\u002F",assetsPath:"\u002Fmahal-doc\u002F_nuxt\u002F",cdnURL:null}}}}("\u002Fdoc\u002Fstore\u002Fmutation"));
--- _nuxt/static/1647495566/docs/basic/directive/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmahal-doc\u002F_nuxt\u002Fstatic\u002F1647495566",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdocs\u002Fbasic\u002Fdirective",config:{_app:{basePath:"\u002Fmahal-doc\u002F",assetsPath:"\u002Fmahal-doc\u002F_nuxt\u002F",cdnURL:a}}}}(null));
