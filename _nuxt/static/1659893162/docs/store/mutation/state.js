dbf36f1faf77d712f49dd9b82cc9465369c8db19
ujjwalguptaofficial <ujjwalkumargupta4u@gmail.com>
2022-08-07 17:31:38 +0000

    Deploying to gh-pages from @ ujjwalguptaofficial/mahal-doc@80d89d1a4469010669e8d74dbe36021006c42b10 🚀

--- _nuxt/static/1659893162/docs/store/mutation/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1659893162",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdocs\u002Fstore\u002Fmutation",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
