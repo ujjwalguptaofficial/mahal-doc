9a2e09d6828363af6c858484e73a785bcd8332fc
ujjwalguptaofficial <ujjwalkumargupta4u@gmail.com>
2022-08-31 05:10:57 +0000

    Deploying to gh-pages from @ ujjwalguptaofficial/mahal-doc@32e081531bff0a8cdea910fcbba7274c2a4360b3 🚀

---
+++ _nuxt/static/1661922655/docs/advanced/code-splitting/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1661922655",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdocs\u002Fadvanced\u002Fcode-splitting",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
