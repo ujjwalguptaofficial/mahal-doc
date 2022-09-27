cfa85b832e352b487def7a31d811560f36e7e32e
ujjwalguptaofficial <ujjwalkumargupta4u@gmail.com>
2022-09-27 14:21:02 +0000

    Deploying to gh-pages from @ ujjwalguptaofficial/mahal-doc@78ca9f8d137b6a9f561026221a4d6344f455d194 🚀

--- _nuxt/static/1664209690/docs/advanced/dynamic-component/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1664209690",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdocs\u002Fadvanced\u002Fdynamic-component",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
